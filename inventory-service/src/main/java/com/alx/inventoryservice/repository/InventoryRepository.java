817ab0448245d2a10b09081a68a20d6e1e71b4bc
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-26 16:44:36 -0500

    [ADD]: Inventory MS | findBySkuCodeIn (InventoryRepository)

--- inventory-service/src/main/java/com/alx/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/alx/inventoryservice/repository/InventoryRepository.java
@@ -3,9 +3,13 @@ package com.alx.inventoryservice.repository;
 import com.alx.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
     boolean existsBySkuCode(String skuCode);
+
+    List<Inventory> findBySkuCodeIn(List<String> skuCodeList);
 
 }
