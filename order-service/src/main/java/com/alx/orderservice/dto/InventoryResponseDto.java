afd875abc74e99588ca4a309a3eef99f9cd25ef4
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-26 16:47:09 -0500

    [ADD]: Order MS | InventoryResponseDto

---
+++ order-service/src/main/java/com/alx/orderservice/dto/InventoryResponseDto.java
@@ -0,0 +1,18 @@
+package com.alx.orderservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class InventoryResponseDto {
+
+    private String skuCode;
+    private boolean isInStock;
+
+}
